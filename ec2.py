3de69abe8eb69be78214ebaba1410bc18ef84a54
nakul-virtuecloud <your-new-email@virtuecloud.com>
2025-05-15 20:19:23 +0530

    comment old code

--- ec2.py
+++ ec2.py
@@ -1,6 +1,7 @@
-#testing 
-import boto3
-#print existing s3 buckets 
-ec2 = boto3.resource('ec2')
-for instance in ec2.instances.all():
-    print(instance.id, instance.state)
+# #testing 
+# import boto3
+# #print existing s3 buckets 
+# ec2 = boto3.resource('ec2')
+# for instance in ec2.instances.all():
+#     print(instance.id, instance.state)
+
